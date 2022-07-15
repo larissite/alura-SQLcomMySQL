9533bb48a524dcd2755c11d10d249d343e9c39aa
Larissa <castrodelari@gmail.com>
2022-07-15 16:41:08 -0300

    adicionando primeiros exercícios de criar e apagar banco de dados

---
+++ criandoEApagandoBD.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE SUCOS;
+
+-- comando abaixo foi exibido em outra maneira de criar um banco de dados, de forma direta no workbench: 
+CREATE SCHEMA `sucos2` DEFAULT CHARACTER SET utf8 ;
+
+-- comando para apagar o banco de dados criado:
+
+DROP DATABASE SUCOS;
+DROP DATABASE `sucos2`;
