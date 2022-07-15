ee2c4193c3a345485b9e316436292496b0ef17bb
Larissa <castrodelari@gmail.com>
2022-07-15 17:44:52 -0300

    adicionando exercícios de criar e apagar tabelas

--- criandoEApagandoTabela.sql
+++ criandoEApagandoTabela.sql
@@ -1,4 +1,4 @@
--- criando a tabela de cadastro de clientes, conforme proposto no exercício das aulas.
+-- criando a tabela de cadastro de clientes, conforme proposto no exercício das aulas, pelos comandos SQL.
 CREATE TABLE Cliente
 (CPF VARCHAR(11),
 NOME VARCHAR(100),
@@ -14,3 +14,29 @@ LIMITE_CREDITO FLOAT,
 VOLUME_COMPRA FLOAT,
 PRIMEIRA_COMPRA BIT(1)
 );
+
+-- criando mais uma tabela para o sistema de vendas do exercício, a de vendedores, pelos comandos SQL:
+
+CREATE TABLE Vendedor (
+MATRICULA VARCHAR(5),
+NOME VARCHAR(100),
+PERCENTUAL_COMISSAO FLOAT
+);
+
+-- a tabela "produto", foi criada com o assistente do workbench MYSQL, abaixo segue o comando gerado:
+
+CREATE TABLE `sucos`.`produto` (
+  `PRODUTO` VARCHAR(20) NULL,
+  `NOME` VARCHAR(150) NULL,
+  `EMBALAGEM` VARCHAR(50) NULL,
+  `TAMANHO` VARCHAR(50) NULL,
+  `SABOR` VARCHAR(50) NULL,
+  `PRECO_LISTA` FLOAT NULL);
+
+-- apagando tabelas: para este exemplo foram criadas mais duas tabelas: clientes2 e clientes3.
+-- a tabela clientes2 foi apagada direto pelo workbench, com o "Drop Table" ao clique do mouse.
+--já a tabela clientes 3 foi apagada com o comando abaixo:
+
+USE sucos;
+
+DROP TABLE cliente3;
